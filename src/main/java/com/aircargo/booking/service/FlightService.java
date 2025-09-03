92d513f6b6aead076dc5f5890419b4698c582906
Ashutosh Agarwal <ashutosh1503k@gmail.com>
2025-09-03 09:38:38 +0530

    Update FlightService.java

--- src/main/java/com/aircargo/booking/service/FlightService.java
+++ src/main/java/com/aircargo/booking/service/FlightService.java
@@ -8,12 +8,11 @@ import com.aircargo.booking.repository.FlightRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -27,21 +26,21 @@ public class FlightService {
         Instant endOfDay = departureDate.plusDays(1).atStartOfDay(ZoneOffset.UTC).toInstant();
         List<Flight> directFlight = flightRepository.findDirectFlight(origin, startOfDay, endOfDay, destination);
 
-        List<OneStopFlightResponse> oneStopFlight = new ArrayList<>();
+        Instant maxConnectionTime = departureDate.plusDays(2).atStartOfDay(ZoneOffset.UTC).toInstant();
 
-        List<Flight> firstLeg = flightRepository.findFirstLeg(origin, startOfDay, endOfDay);
+        List<Object[]> results = flightRepository.findOneStopFlights(
+                origin, destination, startOfDay, endOfDay, maxConnectionTime
+        );
 
-        for(Flight first : firstLeg){
-            Instant arrivalStart = first.getArrivalTime();
-            Instant arrivalEnd = arrivalStart.plusSeconds(2 * 24 * 60 * 60); // plus 2 days
-            List<Flight> secondLeg = flightRepository.findSecondLeg(first.getDestination(), arrivalStart, destination, arrivalEnd);
+        List<OneStopFlightResponse> oneStopFlights = new ArrayList<>();
 
-            for(Flight second : secondLeg){
-                oneStopFlight.add(new OneStopFlightResponse(first,second));
-            }
+        for (Object[] row : results) {
+            Flight firstFlight = (Flight) row[0];
+            Flight secondFlight = (Flight) row[1];
+            oneStopFlights.add(new OneStopFlightResponse(firstFlight, secondFlight));
         }
 
-        return new FlightRouteResponse(directFlight,oneStopFlight);
+        return new FlightRouteResponse(directFlight,oneStopFlights);
     }
 
     public Flight createFlight(FlightRequests flightRequest){
@@ -55,6 +54,38 @@ public class FlightService {
                 .build();
 
         return flightRepository.save(flight);
+    }
+
+    public String checkFlight(String origin, String destination, String flightNumber){
+        Flight flight = flightRepository.findByFlightNumber(flightNumber);
+
+        if(!Objects.equals(flight.getOrigin(), origin) && !Objects.equals(flight.getDestination(),destination)){
+            return "No Flight in DB";
+        }
+
+        if(Objects.equals(flight.getOrigin(), origin) && Objects.equals(flight.getDestination(),destination)){
+            return "Direct flight";
+        }else{
+            return "One Stop";
+        }
+    }
+
+    public HashSet<String> possibleFlights(Flight flight,String destination,String origin){
+        Instant startOfDay = flight.getArrivalTime();
+        Instant endOfDay = startOfDay.plus(1, ChronoUnit.DAYS);
+        Instant maxConnectionTime = startOfDay.plus(2,ChronoUnit.DAYS);
+        List<String> flightName = new ArrayList<>();
+        List<Object[]> res = flightRepository.findOneStopFlightsNative(origin,destination,startOfDay,endOfDay,maxConnectionTime);
+
+        for(Object[] o: res){
+            String a1 = (String) o[0];
+            String a2 = (String) o[1];
+            if(a1.equals(flight.getFlightNumber())){
+                flightName.add(a2);
+            }
+        }
+
+        return new HashSet<>(flightName);
     }
 
 }
